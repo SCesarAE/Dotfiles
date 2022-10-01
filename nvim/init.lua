02b1614e182ccb54ca03a0cb3351ef7dd20d512c
SCesarAE <ca.salgado@disroot.org>
2022-10-01 18:50:39 -0500

    first setting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,9 @@
-pcall(require, "impatient")
-
+--pcall(require, "impatient")
 
 --if require "scripts.first_load"() then
 --  return
 --end
 
-require "settings"
+require "settings.settings"
+require "settings.keymaps"
+require "plugin.packer"
